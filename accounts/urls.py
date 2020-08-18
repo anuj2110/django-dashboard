4aace75d0609be0aaf588ece192b6bbb320c75ff
ANUJ TREHAN <atrehan789@gmail.com>
2020-08-18 13:55:37 +0530

    Added CRUD for orders and linked ordering with the customer instead of independent ordering. Used formsets

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.home),
-    path('products',views.product),
-    path('customers',views.customers),
+    path('',views.home,name="home"),
+    path('products',views.product,name="products"),
+    path('customers/<str:pk>/',views.customers,name="customer"),
+    path('create_order/<str:pk>/',views.createOrder,name='create_order'),
+    path('update_order/<str:pk>/',views.updateOrder,name='update_order'),
+    path('delete_order/<str:pk>/',views.deleteOrder,name='delete_order'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,73 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from.forms import OrderForm
+from .models import *
 # Create your views here.
 def home(request):
-    return render(request,'accounts/dashboard.html')
+
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    # totalCustomers = customers.count()
+    totalOrders = orders.count()
+
+    orders_delivered = orders.filter(status = 'Delivered').count()
+    orders_pending = orders.filter(status = 'Pending').count()
+
+    context ={
+        'orders':orders,
+        'customers':customers,
+        'total_orders': totalOrders,
+        'orders_delivered': orders_delivered,
+        'orders_pending': orders_pending
+    }
+    return render(request,'accounts/dashboard.html',context)
 
 def product(request):
-    return render(request,"accounts/products.html")
+    products = Product.objects.all()
+    return render(request,"accounts/products.html",{'products':products})
 
-def customers(request):
-    return render(request,"accounts/customers.html")
+def customers(request,pk):
+    customer = Customer.objects.get(id = pk)
+    orders = customer.order_set.all()
+    context = {
+        'customer':customer,
+        'orders':orders,
+        'total_orders':orders.count()
+    }
+    return render(request,"accounts/customers.html",context)
+
+def createOrder(request,pk):
+    OrderFormSet = inlineformset_factory(Customer,Order,fields=('product','status'),extra=10)
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset = Order.objects.none(),instance=customer)
+    # form = OrderForm(initial={'customer':customer})
+    if request.method=="POST":
+        # form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST,instance=customer)
+        if formset.is_valid():
+            formset.save()
+            return redirect('/')
+    context ={'formset':formset}
+    return render(request,'accounts/order_form.html',context)
+
+def updateOrder(request,pk):
+    order = Order.objects.get(id = pk)
+    form = OrderForm(instance=order)
+
+    if request.method=="POST":
+        form = OrderForm(request.POST,instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context ={'form':form}
+    return render(request,'accounts/order_form.html',context)
+
+def deleteOrder(request,pk):
+    order = Order.objects.get(id = pk)
+    if request.method=="POST":
+        order.delete()
+        return redirect('/')
+    context = {'item':order}
+    return render(request,'accounts/delete.html',context)
